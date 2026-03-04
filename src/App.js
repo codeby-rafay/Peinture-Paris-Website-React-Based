88303f93d6d4ba03a0a6a51e628ccc7bef5e4229
codeby-rafay <rafayali146@gmail.com>
2026-03-04 14:21:33 +0500

    peinture-paris-website

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Stats from "./components/Stats";
+import Features from "./components/Features";
+import Projects from "./components/Projects";
+import Offer from "./components/Offer";
+import ProductsGrid from "./components/ProductsGrid";
+import Feedback from "./components/Feedback";
+import FAQ from "./components/FAQ";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Home id="home" />
+      <Stats />
+      <Features id="about" />
+      <Projects />
+      <Offer />
+      <ProductsGrid id="products" />
+      <Feedback />
+      <FAQ id="faq" />
+      <Contact id="contact" />
+      <Footer />
+    </>
   );
 }
 
